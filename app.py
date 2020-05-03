7daac7df98388d130b4e08fd99e9c87a0bf4ee31
Lucas Weynars da Costa Salles Pereira <lucasweynars@uber.com>
2020-05-03 17:24:56 +1000

    app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+# from helper import search_books, filter_list
+from book_recommender import find_k_similar_books, book_lookup, top_books
+from flask import Flask, render_template, request, jsonify, Response
+import json
+
+# virtualenv book_search
+# source book_search/bin/activate
+# export FLASK_APP=app.py
+# export FLASK_ENV=development
+# flask run
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # app.logger.debug('started')
+    books = top_books(n=20)
+    return render_template('index.html', books=books)
+
+@app.route('/search_book', methods=['GET'])
+def search_book():
+    # Get parameter from the URL
+    search_string = request.args.get('search_field')
+
+    # books = search_books(search)
+    # filtered_books = filter_list(books, ['isbn','cover_i','title','id_goodreads'])
+
+    filtered_books = book_lookup(search_string, n=20)
+    return render_template('index.html', books=filtered_books)
+
+@app.route('/book_recommendations/<id_goodreads>')
+def book_recommendations(id_goodreads):
+	# id_goodreads = request.args.get('id_goodreads')
+	recommended_books = find_k_similar_books(goodreads_book_id=id_goodreads, n=20)
+	return render_template('index.html', books=recommended_books)
+
+
+if __name__ == '__main__':
+    app.run()
